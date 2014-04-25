624d087dbf0066139f5d15085016f0513390f33a
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-04-25 19:54:25 +0700

    Initial, very basic result test

--- test/results.spec.js
+++ test/results.spec.js
@@ -4,7 +4,14 @@ var should = require('should');
 var request = testHelpers.request;
 
 describe('Showing results', function(){
+	var resultPostData = {};
 	beforeEach(function (done) {
+		resultPostData = {
+			questionId : '111',
+			tagString : 'tag 1, tag 2, tag 3',
+			from : '2014-01-01',
+			to : '2020-01-01'
+		};
 		testHelpers.removeAllDocs(done);
 	});
 
@@ -33,4 +40,29 @@ describe('Showing results', function(){
 				.end(done);
 		})();
 	});
+
+	it('filters the results on question', function (done) {
+		co(function *(){
+			var res = yield [
+				testHelpers.votes.insert({ voteValue : 2, questionId : 111}),
+				testHelpers.votes.insert({ voteValue : 3, questionId : 111}),
+				testHelpers.votes.insert({ voteValue : 4, questionId : 222}),
+				testHelpers.votes.insert({ voteValue : 1, questionId : 111})
+			];
+
+			request
+				.post('/results')
+				.send(resultPostData)
+				.expect(200)
+		  		// .expect(function (res) {
+		  		// 	console.log(res.text);
+		  		// 	res.text.should.containEql('<li>2</li>');
+		  		// 	res.text.should.containEql('<li>3</li>');
+		  		// 	res.text.should.containEql('<li>4</li>');
+		  		// })
+				.end(done);
+		})();
+	});
+	it('filters the results on tags');
+	it('filters the results on to and from dates');
 });
