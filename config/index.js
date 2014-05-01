0681944ad746c02e72013701ab8e2fcc7ea3b69f
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-05-01 19:49:17 +0700

    added the user to the config

--- config/index.js
+++ config/index.js
@@ -1,25 +1,30 @@
-var mongoDevUri = process.env.MONGOHQ_URL || "localhost:27017/koaVote_Dev";
-var mongoStageUri = process.env.MONGOHQ_URL || "localhost:27017/koaVote_Test";
-var mongoProdUri = process.env.MONGOHQ_URL || "localhost:27017/koaVote_Prod";
+var mongoProdUri = process.env.MONGOHQ_URL || 'localhost:27017/koaVote_Prod';
+var adminUser = {
+	name : process.env.BASIC_USER || 'marcus',
+	password : process.env.BASIC_USER || 'koavote'
+};
 
 var config = {
-  local: {
-    mode: 'local',
-    port: 3000,
-    mongoUrl: mongoDevUri
-  },
-  staging: {
-    mode: 'staging',
-    port: 4000,
-    mongoUrl: mongoStageUri
-  },
-  prod: {
-    mode: 'prod',
-    port: process.env.PORT || 5000,
-    mongoUrl: mongoProdUri
-  }
+	local: {
+		mode: 'local',
+		port: 3000,
+		mongoUrl: 'localhost:27017/koaVote_Dev',
+		user : adminUser
+	},
+	staging: {
+		mode: 'staging',
+		port: 4000,
+		mongoUrl: 'localhost:27017/koaVote_Test',
+		user : adminUser
+	},
+	prod: {
+		mode: 'prod',
+		port: process.env.PORT || 5000,
+		mongoUrl: mongoProdUri,
+		user : adminUser
+	}
 };
 
 module.exports = function (mode) {
-  return config[mode || process.argv[2] || 'local'] || config.local;
+	return config[mode || process.argv[2] || 'local'] || config.local;
 };
